d5488938a1eca9c783e98b6b5185019745abf8dd
Rami Nassar <rami.nassar@intellisc.com>
2017-10-02 00:49:56 +0400

    Angular

---
+++ src/app/custom-directives/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive,ElementRef,OnInit } from "@angular/core"; 
+
+@Directive({     
+    selector : '[basicHighLightDirective]'
+})
+export class BasicHighLightDirective implements OnInit {
+    constructor(private elementRef: ElementRef) {
+    }
+
+    ngOnInit() {
+        this.elementRef.nativeElement.style.backgroundColor = 'green';
+    }
+}
